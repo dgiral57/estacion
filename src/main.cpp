a10d2d429f92793d5c3df8e730b0312b3f9a14e3
David Giraldo <davidgs96@hotmail.com>
2022-09-27 20:05:41 -0500

    Post request

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include <Arduino.h>
+#include <string.h>
 #include <wifi.h>
 #include <ESP8266HTTPClient.h>
-
+#include <WiFiClient.h>
 #include <sht30h.h>
 
 #define ssid  "TIGO0B9491" // Declare your ID for WiFi connection
@@ -14,6 +15,8 @@ u_int16_t samplingTime = 1000;
 uint32_t sendingTime = 300000;
 Sht30H SensorH(10,1);
 WIFI Wifi(ssid,psw);
+HTTPClient http;
+WiFiClient Client;
 
 void setup() {
 Serial.begin(115200);
@@ -25,8 +28,9 @@ void loop() {
     samplingTimer = millis();
   }
 
-  if ((millis() - sendingTimer > sendingTime) || SensorH.derivate()){
-    Serial.println(SensorH.getValue());
-    sendingTimer = millis();
+  if ((millis() - sendingTimer > sendingTime) || SensorH.derivate()) {
+    http.begin(Client,"http://0.0.0.0:8080/Values");
+    http.addHeader("Content-Type", "application/json");
+    int httpCode = http.POST("{\"api_key\":\"tPmAT5Ab3j7F9\",\"sensor\":\"BME280\",\"value1\":\"24.25\",\"value2\":\"49.54\",\"value3\":\"1005.14\"}");
   }
 }
